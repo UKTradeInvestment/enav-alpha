3136a11529fd033f96f930e1c99132587c762685
Gerald Isabelle <info@geraldisabelle.me>
2016-07-18 14:10:52 +0100

    hide filter form above search results
    remove form
    
    add blank line at end of file

--- apps/markets/urls.py
+++ apps/markets/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'markets'
 
 urlpatterns = [
-    url(r'^$', views.HomepageView.as_view(), name='home'),
-    url(r'^compare$', views.MarketListView.as_view(), name='list'),
+    url(r'^$', views.MarketListView.as_view(), name='list'),
     url(r'^(?P<pk>[0-9]+)/$', views.MarketDetailView.as_view(), name='detail'),
 ]
