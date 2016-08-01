153ea8b6340c1dc4c5b87295a437ec22a9149ff8
David Downes <sourcetree@theukdave.com>
2016-08-01 12:07:11 +0100

    Fixed the solutions choice to be only on homepage Removed solutions 2's details Made solution 3 only have one filter page

--- apps/markets2/urls.py
+++ apps/markets2/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'markets2'
 
 urlpatterns = [
+    url(r'^$', views.RegionChoiceView.as_view(), name='region-choice'),
     url(r'^markets/$', views.MarketListView.as_view(), name='list'),
 ]
