53f522eddfe750a32ec3c7d6b75b65c52e8216d8
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-08-01 14:20:01 +0100

    Add readme for loading solution 1 sample data.
    Pep8 fix.

--- apps/markets1/models.py
+++ apps/markets1/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 class Market(models.Model):
-    pass
+    # Trading name of the marketplace
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # Description of the marketplace suitable for a seller.
+    description = models.CharField(max_length=200, null=True, blank=True)
+    # URL of the market
+    web_address = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
--- apps/markets1/tests.py
+++ apps/markets1/tests.py
@@ -1,3 +1,34 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from .models import Market
 
-# Create your tests here.
+
+def create_market(**variable_data):
+    if 'name' not in variable_data:
+        variable_data['name'] = "Amazon"
+
+    market = Market(**variable_data)
+    market.save()
+    return market
+
+
+class MarketModelTests(TestCase):
+
+    def test_blank_works(self):
+        name = "Amazon"
+        description = "This is a description"
+        market = Market(description=description, name=name)
+        market.save()
+        self.assertEqual(name, str(market))
+
+
+class MarketListTests(TestCase):
+
+    def test_list_no_markets(self):
+        response = self.client.get(reverse('markets1:list'))
+        self.assertContains(response, 'No market options are available', status_code=200)
+
+    def test_list_markets(self):
+        market = create_market()
+        response = self.client.get(reverse('markets1:list'))
+        self.assertContains(response, market.name, status_code=200)
