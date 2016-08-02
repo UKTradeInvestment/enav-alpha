d7fef97dc56738817921be26ab7db3ee2c8f901e
David Downes <sourcetree@theukdave.com>
2016-08-02 12:39:38 +0100

    Basic solution 1 working, list of articles, each one clickable to show the article content, a link for showing marketplaces and displaying those.
    pep8
    
    fix unit tests

--- apps/markets1/admin.py
+++ apps/markets1/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Market
+from .models import Market, Article
 
 admin.site.register(Market)
+admin.site.register(Article)
--- apps/markets1/urls.py
+++ apps/markets1/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'markets1'
 
 urlpatterns = [
-    url(r'^$', views.MarketsView.as_view(), name='list'),
+    url(r'^$', views.ArticleView.as_view(), name='articles'),
+    url(r'^article/(?P<pk>[0-9]+)/$', views.ArticletDetailView.as_view(), name='article'),
+    url(r'^markets/$', views.MarketsView.as_view(), name='markets'),
 ]
